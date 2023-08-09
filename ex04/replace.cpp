093dcbfc70f40be37edca32d92cc4bf47731c3a3
hugo <hugo.f.o.silva@gmail.com>
2023-08-09 14:01:30 +0100

    ex04 done and tested

--- ex04/replace.cpp
+++ ex04/replace.cpp
@@ -1,8 +1,43 @@
 #include "replace.hpp"
 
+std::string	change_ocurrences(std::string arg, std::string line, std::string str)
+{
+	std::string buffer;
+	std::size_t prevPos, pos = 0;
+
+	while (true)
+	{
+		prevPos = pos;
+		pos = arg.find(line, pos);
+		if (pos == std::string::npos)
+			break;
+		buffer.append(arg, prevPos, pos - prevPos);
+		buffer += str;
+		pos += line.size();
+	}
+	buffer.append(arg, prevPos, arg.size() - prevPos);
+	return buffer;
+}
+
 void	replace(char **av){
-	std::ofstream fs;
-	fs.open(av[1]);
-	fs << "ola\n";
-	fs.close();
+	std::ofstream	out;
+	std::ifstream	in;
+	std::string	name, line, arg, toReplace;
+
+	toReplace = av[2];
+	arg = av[3];
+	name = av[1];
+	name.append(".replace");
+ 	in.open(av[1]);
+	out.open(name.c_str());
+	if (in.is_open())
+	{
+		while(std::getline(in, line))
+		{
+			line = change_ocurrences(line, toReplace, arg);
+			out << line << std::endl;
+		}
+		in.close();
+		out.close();
+	}
 }
